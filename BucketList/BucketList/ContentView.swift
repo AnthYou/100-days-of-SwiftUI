8f381900907cb8ad019908331c1e387b586c6c7e
Anthony You <you.anthony@yahoo.com>
2021-03-13 18:37:46 +0100

    completed with challenges

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -9,55 +9,45 @@ import LocalAuthentication
 import MapKit
 import SwiftUI
 
-struct ContentView: View {
+//MARK: - Map View when unlocked
+
+struct UnlockedView: View {
     @State private var centerCoordinate = CLLocationCoordinate2D()
     @State private var locations = [CodableMKPointAnnotation]()
     @State private var selectedPlace: MKPointAnnotation?
     @State private var showingPlaceDetails = false
     @State private var showingEditScreen = false
-    @State private var isUnlocked = false
     
     var body: some View {
         ZStack {
-            if isUnlocked {
-                MapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: locations)
-                    .edgesIgnoringSafeArea(.all)
-                Circle()
-                    .fill(Color.blue)
-                    .opacity(0.3)
-                    .frame(width: 32, height: 32)
-                VStack {
+            MapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: locations)
+                .edgesIgnoringSafeArea(.all)
+            Circle()
+                .fill(Color.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            VStack {
+                Spacer()
+                HStack {
                     Spacer()
-                    HStack {
-                        Spacer()
-                        Button(action: {
-                            // create a new location
-                            let newLocation = CodableMKPointAnnotation()
-                            newLocation.title = "Example Location"
-                            newLocation.coordinate = self.centerCoordinate
-                            self.locations.append(newLocation)
-                            
-                            self.selectedPlace = newLocation
-                            self.showingEditScreen = true
-                        }) {
-                            Image(systemName: "plus")
-                                .padding()
-                                .background(Color.black.opacity(0.75))
-                                .foregroundColor(.white)
-                                .font(.title)
-                                .clipShape(Circle())
-                                .padding(.trailing)
-                        }
+                    Button(action: {
+                        // create a new location
+                        let newLocation = CodableMKPointAnnotation()
+                        newLocation.coordinate = self.centerCoordinate
+                        self.locations.append(newLocation)
+                        
+                        self.selectedPlace = newLocation
+                        self.showingEditScreen = true
+                    }) {
+                        Image(systemName: "plus")
+                            .padding()
+                            .background(Color.black.opacity(0.75))
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .clipShape(Circle())
+                            .padding(.trailing)
                     }
                 }
-            } else {
-                Button("Unlock Places") {
-                    self.authenticate()
-                }
-                .padding()
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .clipShape(Capsule())
             }
         }
         .alert(isPresented: $showingPlaceDetails) {
@@ -99,6 +89,33 @@ struct ContentView: View {
             print("Unable to save data.")
         }
     }
+}
+
+//MARK: - Main View
+
+struct ContentView: View {
+    @State private var isUnlocked = false
+    @State private var showingAuthenticationAlert = false
+    @State private var authenticationError = ""
+    
+    var body: some View {
+        ZStack {
+            if isUnlocked {
+                UnlockedView()
+            } else {
+                Button("Unlock Places") {
+                    self.authenticate()
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .clipShape(Capsule())
+            }
+        }
+        .alert(isPresented: $showingAuthenticationAlert, content: {
+            Alert(title: Text("Authentication Error"), message: Text(self.authenticationError), dismissButton: .default(Text("OK")))
+        })
+    }
     
     func authenticate() {
         let context = LAContext()
@@ -114,14 +131,20 @@ struct ContentView: View {
                         self.isUnlocked = true
                     } else {
                         // error
+                        self.authenticationError = "\(authenticationError?.localizedDescription ?? "Unknown error.")"
+                        self.showingAuthenticationAlert = true
                     }
                 }
             }
         } else {
             // no biometrics
+            self.authenticationError = "Biometrics authentication unavailable."
+            self.showingAuthenticationAlert = true
         }
     }
 }
+
+//MARK: - Preview
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
